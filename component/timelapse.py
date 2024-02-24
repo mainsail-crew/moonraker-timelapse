9930ec6bd0e823fad93b965ca49a1457477a3f78
Andreas Boeckler <andy@boeckler.org>
2024-02-24 14:23:23 +0100

    refractor implemented snapsho via timelapse config option
    example for DSLR via gphoto
    [timelapse]
    park_time: 0.6
    snapshot_cmd: /home/pi/bin/gphoto2_capture.sh
    snapshot_cmd_check: /home/pi/bin/gphoto2_check.sh #optional
    use_snapshot_cmd: True

--- component/timelapse.py
+++ component/timelapse.py
@@ -48,6 +48,7 @@ class Timelapse:
         self.byrendermacro = False
         self.hyperlapserunning = False
         self.printing = False
+        self.snapshotCmdWorking = False
         self.noWebcamDb = False
 
         self.confighelper = confighelper
@@ -71,6 +72,10 @@ class Timelapse:
             'mode': "layermacro",
             'camera': "",
             'snapshoturl': "http://localhost:8080/?action=snapshot",
+            'use_snapshot_cmd': False,
+            'snapshot_cmd': '',
+            'snapshot_cmd_check': '',
+            'snapshot_cmd_waittime': 10.0,
             'stream_delay_compensation': 0.05,
             'gcode_verbose': False,
             'parkhead': False,
@@ -169,6 +174,8 @@ class Timelapse:
 
     async def component_init(self) -> None:
         await self.getWebcamConfig()
+        if self.config['use_snapshot_cmd'] == True:
+            self.snapshotCmdWorking = await self.check_snapshot_cmd()
 
     def overwriteDbconfigWithConfighelper(self) -> None:
         blockedsettings = []
@@ -248,7 +255,7 @@ class Timelapse:
                                                            rotation
                                                            )
 
-        if not self.config['snapshoturl'].startswith('http') and not self.config['snapshoturl'].startswith('cmd:'):
+        if not self.config['snapshoturl'].startswith('http'):
             if not self.config['snapshoturl'].startswith('/'):
                 self.config['snapshoturl'] = "http://localhost/" + \
                                              self.config['snapshoturl']
@@ -460,6 +467,26 @@ class Timelapse:
             logging.exception(msg)
         self.hyperlapserunning = False
 
+    async def check_snapshot_cmd(self) -> bool:
+        if self.config['snapshot_cmd'] == '':
+            return False
+        if self.config['snapshot_cmd_check'] == '':
+            return True
+        cmd = self.config['snapshot_cmd_check']
+        shell_cmd: SCMDComp = self.server.lookup_component('shell_command')
+        scmd = shell_cmd.build_shell_command(cmd, None)
+        try:
+            cmdstatus = await scmd.run(timeout=2., verbose=False)
+        except Exception:
+            logging.exception(f"Error running cmd '{cmd}'")
+            return False
+
+        logging.info(f"check_snapshot_cmd: {cmd} -> {cmdstatus}")
+        if cmdstatus:
+            return True
+        else:
+            return False
+
     async def newframe(self) -> None:
         # make sure webcamconfig is uptodate before grabbing a new frame
         await self.getWebcamConfig()
@@ -472,10 +499,12 @@ class Timelapse:
         framefile = "frame" + str(self.framecount).zfill(6) + ".jpg"
         url = self.config['snapshoturl']
         waittime = 2.
-        if self.config['snapshoturl'].startswith('cmd:'):
-            cmdStart = self.config['snapshoturl'][4:]
-            cmd = cmdStart + " " + self.temp_dir + framefile
-            waittime = 10.
+        if self.config['use_snapshot_cmd'] == True and not self.snapshotCmdWorking:
+            logging.info(f"snapshot_cmd_check failed on startup or snapshot_cmd is empty: {self.config['snapshot_cmd']}")
+
+        if self.config['use_snapshot_cmd'] == True and self.snapshotCmdWorking:
+            cmd = self.config['snapshot_cmd'] + " " + self.temp_dir + framefile
+            waittime = self.config['snapshot_cmd_waittime']
         else:
             cmd = "wget " + options + self.config['snapshoturl'] \
                 + " -O " + self.temp_dir + framefile
@@ -519,6 +548,10 @@ class Timelapse:
             # print_started
             self.cleanup()
             self.printing = True
+            self.snapshotCmdWorking = False
+
+            if self.config['use_snapshot_cmd'] == True:
+                self.snapshotCmdWorking = await self.check_snapshot_cmd()
 
             # start hyperlapse if mode is set
             if self.config['mode'] == "hyperlapse":
